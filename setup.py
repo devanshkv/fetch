0ba113587de12c14e64d08cc54376937856d606d
Devansh Agarwal <devansh.kv@gmail.com>
2019-02-18 10:37:40 +0100

    Adding candmaker.py as an executable.

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(
     name='fetch',
-    version='0.1.0',
+    version='0.1.5',
     packages=['fetch'],
-    scripts=['bin/predict.py', 'bin/train.py'],
+    scripts=['bin/predict.py', 'bin/train.py', 'bin/candmaker.py'],
     url='https://github.com/devanshkv/fetch',
     license='GNU General Public License v3.0',
     author=['Devansh Agarwal', 'Kshitij Aggarwal'],
