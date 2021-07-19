7a95a2375a5f5e5345b303fdb2d8c8729ce125e8
Devansh Agarwal <da0017@mix.wvu.edu>
2021-07-19 16:36:40 -0400

    installation based bug fixes

--- setup.py
+++ setup.py
@@ -1,18 +1,14 @@
 from setuptools import setup
 
-with open("requirements.txt") as f:
-    required = f.read().splitlines()
-
 setup(
     name="fetch",
     version="0.2.0",
     packages=["fetch"],
-    scripts=["bin/predict.py", "bin/train.py", "bin/candmaker.py"],
+    scripts=["bin/predict.py", "bin/train.py"],
     package_dir={"fetch": "fetch"},
     package_data={"fetch": ["models/model_list.csv", "models/*/*yaml"]},
     url="https://github.com/devanshkv/fetch",
     tests_require=["pytest", "pytest-cov"],
-    install_requires=required,
     license="GNU General Public License v3.0",
     author=["Devansh Agarwal", "Kshitij Aggarwal"],
     author_email=["devansh.kv@gmail.com", "ka0064@mix.wvu.edu"],
